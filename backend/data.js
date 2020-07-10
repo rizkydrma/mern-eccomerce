08aee091d4044bfec5f679c407df9d9ac315f766
unknown <rizky.darmarazak@gmail.com>
2020-07-10 17:35:01 +0700

    make a node server

---
+++ backend/data.js
@@ -0,0 +1,100 @@
+export default {
+  products: [
+    {
+      _id: 1,
+      name: "Slim Shirt Men",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 2,
+      name: "Kemeja Bagus",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 3,
+      name: "Kemeja Hitam",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 4,
+      name: "Slim Shirt Men",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 5,
+      name: "Slim Shirt Men",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 6,
+      name: "Slim Shirt Men",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 7,
+      name: "Slim Shirt Men",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+    {
+      _id: 8,
+      name: "Slim Shirt Men",
+      category: "Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum dolores adipisci? Debitis dolor, aut quo, officiis, nisi nobis porro dicta tenetur magnam blanditiis itaque sint cupiditate pariatur distinctio consequuntur.",
+    },
+  ],
+};
